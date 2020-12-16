7cf20b702eebf3bab7a51b11cb060b97af5b267d
= <liampercy123@gmail.com>
2020-12-16 17:28:35 +1000

    Fixing issues with typings on build

--- src/Projects/StudySaviour.tsx
+++ src/Projects/StudySaviour.tsx
@@ -16,6 +16,7 @@ export const StudySaviour = () => {
 					<ScrollAnimation
 						animateIn='fadeInLeft'
 						animateOnce
+						// @ts-ignore
 						afterAnimatedIn={() => setAnimateCarousel(true)}
 						className='project-image-container'>
 						<Carousel
